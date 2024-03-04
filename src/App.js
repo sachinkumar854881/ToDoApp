dde2e492533a2de3fdcc2e13d34cf36eab9cc63a
sachin <sachinkumar854881@gmail.com>
2024-03-04 13:53:36 +0530

    add files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {useState, useRef} from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const data = localStorage.getItem('lists') ? JSON.parse(localStorage.getItem('lists')) : [];
+
+  const [list, setList] = useState(data);// yaha array mein  hum by default value bhi de sakte hai 
+  const newtask= useRef("")
+  const [search,setSearch] = useState('');
+  console.log("re-render")
+
+  function submitData()
+  {
+    localStorage.setItem('lists',JSON.stringify([...list, newtask.current.value]))
+    setList([...list, newtask.current.value])
+    newtask.current.value=""
+
+  }
+  // how update a add task in a list
+  function updateTask(e,i)
+  {
+    const uptask = [...list]
+    uptask.splice(i, 1, e.target.value)
+    setList(uptask)
+    localStorage.setItem('lists',JSON.stringify(uptask))
+  }
+  // how delete a task in list
+  function deleteTask(i)
+  {
+    const deltask = [...list]
+    deltask.splice(i, 1)
+    setList(deltask)
+    localStorage.setItem('lists',JSON.stringify(deltask))
+  }
+  
+
+  return(
+    <div className='app'>
+      
+      <div className="search">
+        <input type="text" placeholder="Search task🔎" onChange={(e)=>{setSearch(e.target.value)}}/>
+      </div>
+
+      <h1 className='heading'>TO-DO App ⚡</h1>
+      
+      <div className='inputs'>
+        <input type="text" ref={newtask} />
+        <button className='btn' onClick={submitData}>Add Task✔</button>  
+      </div>
+   
+      <div className="container"> 
+        {
+
+          list.map((val,i) => {
+            if(val.toLowerCase().includes(search.toLowerCase())){
+              return(
+                <div className="list">
+                  <input type="text" value={val} onChange={updateTask}/>
+                  <span className="icon" onClick={deleteTask}>❌</span>
+                </div>
+              )
+            }
+          })
+          
+        }
+      </div>
     </div>
   );
 }
